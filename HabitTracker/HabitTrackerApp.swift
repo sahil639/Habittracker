4d682de52ce0d65d69b86ef898877cf4c32774f3
sahil639 <sahilpednekar332000@gmail.com>
2025-01-28 19:04:04 +0400

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Sahil Pednekar on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
